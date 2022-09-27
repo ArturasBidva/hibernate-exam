650bde006221b7bceda11177cc5571b5a5b4d296
ArturasBidva <100302918+ArturasBidva@users.noreply.github.com>
2022-09-27 17:40:31 +0300

    Update Validator.java

--- src/main/java/lt/arturas/exam/application/utils/Validator.java
+++ src/main/java/lt/arturas/exam/application/utils/Validator.java
@@ -1,8 +1,5 @@
 package lt.arturas.exam.application.utils;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Validator {
     public boolean hasDigits(String input) {
         char[] letters = input.toCharArray();
@@ -14,9 +11,4 @@ public class Validator {
         return false;
     }
 
-    public boolean containsIllegals(String toExamine) {
-        Pattern pattern = Pattern.compile("[~#@*+!%{}<>\\[\\]|\"\\_^]");
-        Matcher matcher = pattern.matcher(toExamine);
-        return matcher.find();
-    }
 }
